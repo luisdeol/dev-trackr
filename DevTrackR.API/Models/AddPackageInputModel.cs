73dfb6117cbf995abd7e699da70dc5684ae22cae
Luis Felipe <luisfelipekai@gmail.com>
2022-05-12 22:22:39 -0300

    Aula 4 e 5.

--- DevTrackR.API/Models/AddPackageInputModel.cs
+++ DevTrackR.API/Models/AddPackageInputModel.cs
@@ -9,5 +9,7 @@ namespace DevTrackR.API.Models
     {
         public string Title { get; set; }
         public decimal Weight { get; set; }
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
     }
 }
